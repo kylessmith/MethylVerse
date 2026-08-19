12d42f5daebd77882390d0d595e66df299d16e76
Smith <ksmith10@eu-l241740.stjude.sjcrh.local>
2026-08-19 14:09:16 -0500

    general updates

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ try:
     with open(init_py_path) as f:
         for line in f:
             if line.startswith('__version__'):
-                version = ast.parse(line).body[0].value.s
+                version = ast.literal_eval(ast.parse(line).body[0].value)
                 break
         else:
             print( "WARNING: Version information not found in '%s', using placeholder '%s'" % (init_py_path, version), file=sys.stderr )
